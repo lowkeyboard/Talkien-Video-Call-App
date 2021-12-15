8d03f6cc9c56feaa5332c125471b4bd4a3563082
lowkeyboard <caqlarybak@gmail.com>
2021-12-16 01:23:00 +0300

    firebase pods added

--- Talkien/Talkien/App/AppDelegate.swift
+++ Talkien/Talkien/App/AppDelegate.swift
@@ -1,12 +1,17 @@
 
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        //init firebase here:
+        FirebaseApp.configure()
+        
+        //navigating here: 
         app.router.start()
         return true
     }
